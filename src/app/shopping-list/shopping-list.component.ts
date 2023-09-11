bec9417caa0ad2885148a286f74405667646a16f
katsup <brett_7y7@yahoo.ca>
2023-09-11 17:40:35 +0900

    Set active link in saved lists

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,7 @@ import { ShoppingList } from '../interfaces/shoppingLists';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   shoppingLists: ShoppingList[];
+  shoppingListId: string = '';
   shoppingListName: string;
   shoppingListSaverIsOpen = false;
   @ViewChild('f') listNameForm: NgForm;
@@ -56,7 +57,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   // new implementation
   onLoadList(shoppingListId: number|string){
     this.alert = { isCallingApi: true, message: 'Loading list...'};
-    const { ingredients } = this.getShoppingListById(shoppingListId);
+    const { id, ingredients } = this.getShoppingListById(shoppingListId);
+    this.shoppingListId = id;
     this.ingredients = ingredients;
     this.slService.setIngredients(ingredients);
     
